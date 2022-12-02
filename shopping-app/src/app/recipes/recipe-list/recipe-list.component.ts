e7259758d6977539db2640005b03e24196323a8a
stefanbanu <stefanbanu1@gmail.com>
2022-12-02 14:21:34 +0200

    Created a model and used to display the recipes.

--- shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is just a test.', 'https://www.simplyrecipes.com/thmb/2cLydgR2kWyktf7JpKu7ro-gH5M=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Easy-Sauteed-Spinach-LEAD-13-0822e33a30f74624a3664f46eee2f4a7.jpg'),
+     new Recipe('A Test Recipe', 'This is just a test.', 'https://www.simplyrecipes.com/thmb/2cLydgR2kWyktf7JpKu7ro-gH5M=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Easy-Sauteed-Spinach-LEAD-13-0822e33a30f74624a3664f46eee2f4a7.jpg' )
+  ];
 
   constructor() { }
 
